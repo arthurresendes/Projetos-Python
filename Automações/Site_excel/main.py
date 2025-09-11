9c0203040e120e9c21d36493cc9aca96eb8aa004
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-11 16:35:05 -0300

    atualização

--- Automações/Site_excel/main.py
+++ Automações/Site_excel/main.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="Orçamento")
 
 with st.container():
     st.title("Tabela valores")
-    df = pd.read_excel("Treinamento.xlsx")
+    df = pd.read_excel("Treinamento.xlsx" , skiprows=1)
     st.write(df.head())
+
 

8f38fdcd40e418d40e3277e16882888d61e33e38
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-12 00:43:40 -0300

    atualização

---
+++ Automações/Conversão_df/main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+import requests
+
+requisicao = requests.get("https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL")
+dic_requisi = requisicao.json()
+
+st.set_page_config(page_title="Conversão")
+
+with st.container():
+    st.title("Dolar , Euro e Bitcoin em tempo real")
+    df = pd.read_excel("Treinamento.xlsx" , skiprows=1, skipfooter=1)
+    dadosUSD = dic_requisi['USDBRL']
+    dadosEUR = dic_requisi['EURBRL']
+    dadosBTC = dic_requisi['BTCBRL']
+    conversaoDOL = dadosUSD['bid']
+    conversaoEUR = dadosEUR['bid']
+    conversaoBTC = dadosBTC['bid']
+    
+
+    st.subheader("Planilha valores: ")
+    st.data_editor(df)
+    df['Total Dolar'] = df['TOTAL'] / float(conversaoDOL)
+    df['Total Euro'] = df['TOTAL'] / float(conversaoEUR)
+    df['Total Bitcoin'] = df['TOTAL'] / float(conversaoBTC)
+    
+    st.subheader(f"Dolar em tempo real: {conversaoDOL}")
+    st.write("Conversão: ")
+    st.write(df[['CONTAS','Total Dolar']])
+    st.write("\n")
+    st.subheader(f"Euro em tempo real: {conversaoEUR}")
+    st.write("Conversão: ")
+    st.write(df[['CONTAS','Total Euro']])
+    st.write("\n")
+    st.subheader(f"Bitcoin em tempo real: {conversaoBTC}")
+    st.write("Conversão: ")
+    st.write(df[['CONTAS','Total Bitcoin']])
--- Automações/Site_excel/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-st.set_page_config(page_title="Orçamento")
-
-with st.container():
-    st.title("Tabela valores")
-    df = pd.read_excel("Treinamento.xlsx" , skiprows=1)
-    st.write(df)

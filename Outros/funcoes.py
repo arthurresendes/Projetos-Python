b150b1759f39ab1aaa2e07447338dc621fc01888
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-21 12:49:06 -0300

    atualização

--- Outros/funcoes.py
+++ Outros/funcoes.py
@@ -1,7 +1,7 @@
 lista = [1,2,3,4,1,6,1]
 novaLitsa = []
 
-def removendo_repetidos():
+def removendo_repetidos() -> None:
     for i in lista:
         if i not in novaLitsa:
             novaLitsa.append(i)
@@ -10,7 +10,7 @@ def removendo_repetidos():
 lista2 = [0,1,2,3,0,0,1]
 novaLista2 = []
 
-def removendo_zero():
+def removendo_zero() -> None:
     contador = 0
     for i in lista2:
         if i == 0 and contador == 0 or i != 0:
@@ -23,8 +23,11 @@ def removendo_zero():
 def palindromo(palavra: str) -> str:
     return palavra[::-1]
 
-if __name__ == "__main__":
+def main():
     removendo_repetidos()
     removendo_zero()
     palavra = input("Digite uma palavra: ")
     print(palindromo(palavra))
+
+if __name__ == "__main__":
+    main()

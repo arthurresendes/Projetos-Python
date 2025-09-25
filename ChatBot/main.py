61b6c7021315c8b74f2f8d661d5d86c68e9f8885
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-25 16:22:04 -0300

    atualizar

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -7,7 +7,7 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 # Criar cliente
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key=api_key) # Sua API carrega aqui
 
 def criar_chatbot(mensagem):
     resposta = client.chat.completions.create(

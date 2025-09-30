b3cd1ccd9d06718ba7a1dcbc56a1f1d0f854e396
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-30 19:55:41 -0300

    atualização

--- Help Desk/main.py
+++ Help Desk/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 app = FastAPI()
 # uvicorn main:app --reload
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated=["auto"])
 
 from auth_routes import auth_router
 from outher_routes import outher_router

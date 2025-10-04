fd91adc0351d209defd128a6a6b2bc430c7cc117
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-04 14:34:41 -0300

    atualização

--- Sistema_Imobiliario/main.py
+++ Sistema_Imobiliario/main.py
@@ -24,6 +24,12 @@ class Imovel():
             return "Valo invalido"
         else:
             self.__preco = novo_valor
+    
+    def descricao(self):
+        if self.disponivel:
+            return f"Casa no endereço {self.endereco} no valor de {self.__preco} com a área de {self.area} m² e disponivel"
+        else:
+            return f"Casa no endereço {self.endereco} no valor de {self.__preco} com a área de {self.area} m² não disponivel"
 
 class Casa(Imovel):
     def __init__(self,endereco:str,area:float,preco:float,disponivel:True,num_quartos:int , num_vagas:int , quintal:bool):
@@ -31,6 +37,11 @@ class Casa(Imovel):
         self.numQuarto = num_quartos
         self.numVaga = num_vagas
         self.quintal = quintal
+    
+    def descricao(self):
+        return (f"Casa com {self.num_quartos} quartos, {self.num_vagas} vagas, "
+                f"{'com quintal' if self.quintal else 'sem quintal'}, "
+                f"localizada em {self.endereco}. Preço: R${self.ver_preco():,.2f}")
 
 class Apartamento(Imovel):
     def __init__(self,endereco:str,area:float,preco:float,disponivel:True,andar:int , valor_condominio:int ,  varanda:bool):

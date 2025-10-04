075c17c97a0138b2161451319e893a5887d77d87
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-04 14:11:50 -0300

    atualização

---
+++ Sistema_Imobiliario/main.py
@@ -0,0 +1,15 @@
+'''
+Classes: Imovel, Casa, Apartamento, Corretor, Cliente.
+
+Mostre como usar:
+
+Herança (Casa e Apartamento herdam de Imovel);
+
+Encapsulamento (preço privado);
+
+Polimorfismo (método descricao() diferente);
+
+Composição (Corretor vende Imovel para Cliente).
+
+'''
+

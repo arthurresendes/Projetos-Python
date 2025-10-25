738f614970b2e674600694779688df186f32d4fd
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-25 15:01:44 -0300

    atualizaçaõ

--- Outros/escolaPOO.py
+++ Outros/escolaPOO.py
@@ -9,8 +9,24 @@ class Pessoa():
 
 
 class Aluno(Pessoa):
-    pass
+    def __init__(self,nome:str,idade:int,ano_escolar: int,ensino:str):
+        self.ano_escolar = ano_escolar
+        self.ensino = ensino
+        super().__init__(nome,idade)
+    
+    def descricao(self) -> str:
+        return f"O aluno(a) com {self.nome} e com {self.idade} anos esta no {self.ano_escolar} ano no ensino {self.ensino}"
     
 
 class Professor(Pessoa):
-    pass
+    def __init__(self,nome:str,idade:int,ano_escolar: int,ensino:str,materia:str):
+        self.ano_escolar = ano_escolar
+        self.ensino = ensino
+        self.materia = materia
+        super().__init__(nome,idade)
+    
+    def descricao(self) -> str:
+        return f"O professor/professora com {self.nome} e com {self.idade} anos da aula de {self.materia} no {self.ano_escolar} ano no ensino {self.ensino}"
+
+class Aula():
+    

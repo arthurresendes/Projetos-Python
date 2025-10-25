15b9e35cd9be05895d71eda4c76980eb6e025237
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-25 15:13:22 -0300

    atualização

--- Outros/escolaPOO.py
+++ Outros/escolaPOO.py
@@ -14,7 +14,7 @@ class Aluno(Pessoa):
         super().__init__(nome,idade)
     
     def descricao(self) -> str:
-        return f"O aluno(a) com {self.nome} e com {self.idade} anos esta no {self.ano_escolar} ano no ensino {self.ensino}"
+        return f"O aluno(a) com nome de {self.nome} e com {self.idade} anos esta no {self.ano_escolar} ano no ensino {self.ensino}"
     
 
 class Professor(Pessoa):
@@ -25,7 +25,7 @@ class Professor(Pessoa):
         super().__init__(nome,idade)
     
     def descricao(self) -> str:
-        return f"O professor/professora com {self.nome} e com {self.idade} anos da aula de {self.materia} no {self.ano_escolar} ano no ensino {self.ensino}"
+        return f"O professor/professora com o nome de {self.nome} tem {self.idade} anos, da aula de {self.materia} no {self.ano_escolar} ano no ensino {self.ensino}"
 
 class Aula():
     def __init__(self,aluno:Aluno, professor: Professor):
@@ -34,17 +34,17 @@ class Aula():
     
     def ver_relacao_aluno_professor(self):
         if self.aluno.ano_escolar == self.professor.ano_escolar and self.aluno.ensino.lower() == self.professor.ensino.lower():
-            return f"O professor {self.professor.nome} da aula para o aluno(a) {self.aluno} na materia {self.professor.materia}"
+            return f"O professor {self.professor.nome} da aula para o aluno(a) {self.aluno.nome} na materia {self.professor.materia}"
         else:
-            return  f"O professor não {self.professor.nome} da aula para o aluno(a) {self.aluno}"
+            return  f"O professor {self.professor.nome} não da aula para o aluno(a) {self.aluno.nome}"
 
 
 if __name__ == "__main__":
     aluno1 = Aluno('Arthur Resende', 16, 2, 'Medio')
     print(aluno1.descricao())
-    professor1 = Professor('Sandra', 53, 3, 'Medio')
+    professor1 = Professor('Sandra', 53, 3, 'Medio', 'Matematica')
     print(professor1.descricao())
-    professor2 = Professor('Marcu Todday' , 61, 2, 'Medio')
+    professor2 = Professor('Marcus Todday' , 61, 2, 'Medio', 'Fisica')
     print(professor2.descricao())
     
     aula1 = Aula(aluno1,professor1)

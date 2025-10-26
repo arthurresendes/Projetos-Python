d7a0cd67b8f3fef0bcebad8d7013d1620fdea733
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2025-10-26 20:20:58 -0300

    Delete Help Desk directory

--- Help Desk/outher_routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-outher_router = APIRouter(prefix="/chamado", tags=['chamado'])
-
-
-@outher_router.get("/")
-async def vizualizar_chamados():
-    return {"chamado": "Indisponivel"}

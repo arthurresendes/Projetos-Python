15a638c6d9063b22f8c73560347615d34f43730e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-26 22:30:58 -0300

    atualização

--- Rede_Neural/sample_data/coleta.py
+++ Rede_Neural/sample_data/coleta.py
@@ -8,28 +8,26 @@ from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalFormatter # Formata json de forma legivel no terminal
 from google_play_scraper import Sort, reviews, app # Coleta dados da PlayStore sem precisar de api oficial
 
-sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+sns.set(style='whitegrid', palette='muted', font_scale=1.2) # Estilo do grafico com fundo branco com grade discreta , cores suaves e font 
 
 apps_ids = ['br.com.brainweb.ifood', 'com.cerveceriamodelo.modelonow',
 'com.mcdo.mcdonalds', 'habibs.alphacode.com.br',
-'com.ubercab.eats', 'com.grability.rappi','burgerking.com.br.appandroid','com.vanuatu.aiqfome']
+'com.ubercab.eats', 'com.grability.rappi','burgerking.com.br.appandroid','com.vanuatu.aiqfome'] # apps vindo da playstore
 
-app_infos = []
-app_erros = []
+app_infos = [] # Armazena as informações dos apps no append
 for ap in tqdm(apps_ids):
     try:
         info = app(ap,lang='pt', country='br')
-        del info['comments']
+        del info['comments'] # Deleta os comentarios do app
         app_infos.append(info)
     except:
-        app_erros.append(ap)
         pass
 
-app_infos_df = pd.DataFrame(app_infos)
-print(app_infos_df.head(7))
+app_infos_df = pd.DataFrame(app_infos) # Dataframe com as informações do app
+print(app_infos_df.head(7)) # Mostra as primeiras 7 linhas dos app
 
 
-app_reviews = []
+app_reviews = [] # Vai coletar as revisões
 for ap in tqdm(apps_ids):
     for score in list(range(1,6)):
         for sort_order in [Sort.MOST_RELEVANT, Sort.NEWEST]:
@@ -46,10 +44,11 @@ for ap in tqdm(apps_ids):
                 r['appId'] = ap
             app_reviews.extend(rvs)
 
-df = pd.DataFrame(app_reviews)
-sns.countplot(df.score)
-plt.xlabel('review score')
+df = pd.DataFrame(app_reviews) # cria um dataframe das reviews
+sns.countplot(df.score) # grafico das notas
+plt.xlabel('review score') # label do grafico
 
+# Avaliação dos sentimentos
 def to_sentiment(rating):
     rating = int(rating)
     if rating <= 2:
@@ -59,6 +58,7 @@ def to_sentiment(rating):
     else:
         return 2
 
+# Aplica a dunção a cada linha/coluna para transformar dados sem loops explicitos
 df['sentiment'] = df.score.apply(to_sentiment)
 
 class_names = ['negative', 'neutral' ,'positive']
@@ -66,4 +66,4 @@ class_names = ['negative', 'neutral' ,'positive']
 ax = sns.countplot(df.sentiment)
 plt.xlabel("review sentiment")
 ax.set_xticklabels(class_names)
-df.to_csv('reviews.csv', index=False, header=True)
+df.to_csv('reviews.csv', index=False, header=True) # Salva informações em umcsv

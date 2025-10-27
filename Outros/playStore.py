caae96b9a658e2b70b634399379bc9925d9607f7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-26 22:48:07 -0300

    atualização

--- Outros/playStore.py
+++ Outros/playStore.py
@@ -2,17 +2,22 @@ from google_play_scraper import app
 # https://play.google.com/store/apps/details?id=com.supercell.clashroyale
 # https://play.google.com/store/apps/details?id=com.dts.freefireth
 # https://play.google.com/store/apps/details?id=com.supercell.brawlstars
+# https://play.google.com/store/apps/details?id=com.tencent.ig
+
 # Pegar oq o id recebe e para ver dados em JSON dos app da PlayStore
 
 app_id_clash = 'com.supercell.clashroyale'
 app_id_free_fire = 'com.dts.freefireth'
 app_id_brawl_stars = 'com.supercell.brawlstars'
+app_pubg = 'com.tencent.ig'
 
 info_clash = app(app_id_clash, lang='pt', country='br')
 info_free_fire = app(app_id_free_fire, lang='pt', country='br')
 info_brawl_stars = app(app_id_brawl_stars, lang='pt', country='br')
+info_pubg = app(app_pubg , lang='pt', country='br')
 
-print(f'Jogo:{info_free_fire['title']} com a avaliação em {info_free_fire['score']}')
-print(f'Jogo:{info_clash['title']} com a avaliação em {info_clash['score']}')
-print(f'Jogo:{info_brawl_stars['title']} com a avaliação em {info_brawl_stars['score']}')
+print(f'Jogo: {info_free_fire['title']} com a avaliação em {info_free_fire['score']}')
+print(f'Jogo: {info_clash['title']} com a avaliação em {info_clash['score']}')
+print(f'Jogo: {info_brawl_stars['title']} com a avaliação em {info_brawl_stars['score']}')
+print(f'Jogo: {info_pubg['title']} com a avaliação em {info_pubg['score']}')
 
--- Rede_Neural/sample_data/info.py
+++ Rede_Neural/sample_data/info.py
@@ -1,5 +1,5 @@
 '''
-BERT = É uma tecnica de representação da linguagem desenvolvido por pesquisadores sendo uma tecina de processamento de linguagem natural. O BERT é basicamente uma pilha de codificadores de transformers.
+BERT = É uma tecnica de representação da linguagem desenvolvido por pesquisadores sendo uma tecnina de processamento de linguagem natural. O BERT é basicamente uma pilha de codificadores de transformers.
 
 
 

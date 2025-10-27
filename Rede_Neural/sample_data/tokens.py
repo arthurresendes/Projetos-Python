46a40063a150853a524b1b203edebb2786a4571a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-26 22:41:51 -0300

    atualização

--- Rede_Neural/sample_data/tokens.py
+++ Rede_Neural/sample_data/tokens.py
@@ -1,6 +1,5 @@
-import transformers
-from transformers import BertModel, BertTokenizer, get_linear_schedule_with_warmup
-PRE_TRAINED_MODEL_NAME = 'neuralmind/bert-base-portuguese-cased'
+from transformers import BertModel, BertTokenizer, get_linear_schedule_with_warmup # BertTokenizer -> Converte texto em tokens, BertModel -> Modelo bert pré treinado usado para gerar representações vetoriais
+PRE_TRAINED_MODEL_NAME = 'neuralmind/bert-base-portuguese-cased' # Modelo pré-treinado em português , cased -> diferencia maiusculas de minusuculas , preservando informações como nomes proprios
 from torch.optim import AdamW
 tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME) # modelo BERT para português (BERTimbau cased), Carrega vocabulário e regras de tokenização do BERTimbau.
 

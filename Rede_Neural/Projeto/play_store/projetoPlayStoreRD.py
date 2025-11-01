d3d1f45bf9201d3c37c478f6b1e0761c740253d9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-01 12:27:52 -0300

    atualização

--- Rede_Neural/Projeto/play_store/projetoPlayStoreRD.py
+++ Rede_Neural/Projeto/play_store/projetoPlayStoreRD.py
@@ -1,8 +1,29 @@
+import json
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+from google_play_scraper import Sort, reviews, app
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from collections import defaultdict, Counter
+import re
+
+# 1 -- Conifgs Iniciais
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
 
 
-
-
-
+# 
 
 aplicativos_play = [
     "br.com.drogaraia",

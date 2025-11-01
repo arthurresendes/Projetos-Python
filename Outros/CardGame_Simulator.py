9821561815d98a01f638f573be41edbbe600b617
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-01 13:41:37 -0300

    atualização

--- Outros/CardGame_Simulator.py
+++ Outros/CardGame_Simulator.py
@@ -25,3 +25,26 @@ class Baralho:
     def __init__(self, cartas: list[Carta], nome_baralho: str):
         self._cartas = cartas
         self._nome_baralho = nome_baralho
+
+class Mao:
+    def __init__(self, cartas_na_mao : list[Carta], limite_cartas : int):
+        self._cartas_na_mao = cartas_na_mao
+        self._limite_cartas = limite_cartas
+
+class Jogador:
+    def __init__(self,nome:str,vida_atual:int,mana_max:int , mana_atual:int, baralho_principal: Baralho, mao_jogador: Mao):
+        self._nome = nome
+        self._vida_atual = vida_atual
+        self._mana_max = mana_max
+        self._mana_atual = mana_atual
+        self._baralho = baralho_principal
+        self._mao_jogador = mao_jogador
+
+class MesaJogo:
+    def __init__(self, jogador: list[Jogador] ,turno_atual:int, jogador_ativo:Jogador,campo_batalha: dict ):
+        self._jogador = jogador
+        self._turno_atual = turno_atual
+        self._jogador_ativo = jogador_ativo
+        self._campo_batalha = campo_batalha
+
+

c055efb8a13b5bfdc85f6a158739e240d95d321f
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-18 19:10:51 -0300

    ChatBot Exemplo, sem creditos na api openia

--- ChatBot/chatgpt.py
+++ ChatBot/chatgpt.py
@@ -4,21 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def generate_text(prompt_text):
+def chatgpt(prompt_text):
     try:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
+        resposta = cliente.chat.completions.create(
+            model="gpt-5o-mini",
             messages=[
-                {"role": "system", "content": "Você é um assistente prestativo e conciso."},
+                {"role": "system", "content": "Você é um assistente prestativo e busca explicar da melhor maneira possível, onde qualquer tipo de pessoa entenda."},
                 {"role": "user", "content": prompt_text}
             ],
             max_tokens=150,
             temperature=0.7,
         )
         
-        return response.choices[0].message.content.strip()
+        return resposta.choices[0].message.content.strip()
     
     except Exception as e:
         return f"Ocorreu um erro: {e}"
@@ -28,11 +28,13 @@ if __name__ == "__main__":
     while True:
         prompt = input("Digite sair para sair!! \nFaça sua pergunta: ").lower()
         if prompt == 'sair':
+            print("Saindo...")
             break
         else:
-            resposta = generate_text(prompt)
+            resposta = chatgpt(prompt)
             if "Ocorreu um erro" in resposta:
                 print("Sem creditos em sua API, recarregue devidamenete")
+                print("Saindo...")
                 break
             else:
                 print(f"Resposta do assistente: {resposta}")

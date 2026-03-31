5119297ad449a02e364d7ef613ce0062b1ddab09
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-31 16:44:21 -0300

    See action in real time with python

---
+++ API_Acoes/main.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+token = os.getenv("TOKEN_ACTIONS")
+
+def menu():
+    print("="*50)
+    print("Aqui você podera ver valores de determinada ação!!")
+    print("="*50)
+
+def nome_acao() -> str:
+    nome = input("Digite o codigo de identificação da ação: ").upper()
+    return nome
+
+def validacao():
+    ticker = nome_acao()
+    url = f"https://brapi.dev/api/quote/{ticker}?token={token}"
+    response  = requests.get(url)
+    data = response.json()
+    
+    if response.status_code == 200:
+        preco = data['results'][0]['regularMarketPrice']
+        nome_completo = data['results'][0]['longName']
+        print("Nome | Preço")
+        print(f"{nome_completo}: R$ {preco}")
+    else:
+        print("Erro ao achar a ação")
+
+if __name__ == "__main__":
+    menu()
+    validacao()

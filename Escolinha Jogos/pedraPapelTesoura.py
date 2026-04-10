280f6f16cd351c92404b6e15e34103a6526c4bc9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-10 15:58:24 -0300

    Clean Code

--- Escolinha Jogos/pedraPapelTesoura.py
+++ Escolinha Jogos/pedraPapelTesoura.py
@@ -1,6 +1,10 @@
 from random import choice
-
 import time
+
+contadorUser1 = 0
+contadorUser2 = 0
+contadorUser = 0
+contadorProg = 0
 
 def modo_jogo():
     introducao()
@@ -33,45 +37,43 @@ def introducao():
     print("------------------------------------")
     time.sleep(5)
 
+def result(result: str, modo: str):
+    global contadorUser1
+    global contadorUser2
+    global contadorUser
+    global contadorProg
+    
+    if modo == "persons":
+        if result == "pedra x tesoura" or  result == "papel x pedra" or result == "tesoura x papel":
+            contadorUser1+=1
+            print("User 1 venceu")
+        elif result == "tesoura x pedra" or  result == "pedra x papel" or result == "papel x tesoura":
+            contadorUser2 += 1
+            print("User 2 venceu")
+        else:
+            print("Resposta incopativel")
+    else:
+        if result == "pedra x tesoura" or  result == "papel x pedra" or result == "tesoura x papel":
+            contadorUser+=1
+            print("Você venceu")
+        elif result == "tesoura x pedra" or  result == "pedra x papel" or result == "papel x tesoura":
+            contadorProg += 1
+            print("Você perdeu")
+        else:
+            print("Resposta incopativel")
 
 def gameUsers():
-    contadorUser1 = 0
-    contadorUser2 = 0
+    global contadorUser1
+    global contadorUser2
     while True:
         user1 = input("User 1 -> Pedra | Papel | Tesoura: ").lower()
         user2 = input("User 2 -> Pedra | Papel | Tesoura: ").lower()
 
-        print(f"{user1} x {user2}")
-
-        if user1 == 'pedra' and user2 == 'papel':
-            print("User 2 venceu!!")
-            contadorUser2 += 1
-        elif user1 == 'pedra' and user2 == 'pedra':
-            print("Empate!!")
-        elif user1 == 'pedra' and user2 == 'tesoura':
-            print("User 1!!")
-            contadorUser1 += 1
-
-        elif user1 == 'papel' and user2 == 'papel':
-            print("Empate!!")
-        elif user1 == 'papel' and user2 == 'tesoura':
-            print("User 2 venceu!!")
-            contadorUser2 += 1
-        elif user1 == 'papel' and user2 == 'pedra':
-            print("User 1 venceu!!")
-            contadorUser1 += 1
-
-        elif user1 == 'tesoura' and user2 == 'tesoura':
-            print("Empate!!")
-        elif user1 == 'tesoura' and user2 == 'papel':
-            print("User 1 venceu!!")
-            contadorUser1 += 1
-        elif user1 == 'tesoura' and user2 == 'pedra':
-            print("User 2 venceu!!")
-            contadorUser2 += 1
-
+        if user1 == user2:
+            print("Empate")
         else:
-            print("Resposta incompatível!!")
+            res = f"{user1} x {user2}"
+            result(res,"persons")
 
         print(f"Pontos user 1 -> {contadorUser1} \nPontos user 2 -> {contadorUser2}")
 
@@ -84,48 +86,23 @@ def gameUsers():
 
         opcao = input("Digite 0 se quiser sair ou qualquer outra tecla se deseja continuar: ")
         if opcao == "0":
-            print("Até a próxima!")
+            print("Jogo encerrado... Até a próxima!")
             break
 
 def gamePc():
-    contadorUser = 0
-    contadorProg = 0
+    global contadorUser
+    global contadorProg
+
     while True:
         jogo = ['pedra', 'papel', 'tesoura']
         programa = choice(jogo)
         user = input("Pedra | Papel | Tesoura: ").lower()
-        print(f"{user} x {programa}")
-
-        if user == 'pedra' and programa == 'papel':
-            print("Você perdeu!!")
-            contadorProg += 1
-        elif user == 'pedra' and programa == 'pedra':
-            print("Empate!!")
-        elif user == 'pedra' and programa == 'tesoura':
-            print("Você venceu!!")
-            contadorUser += 1
-
-        elif user == 'papel' and programa == 'papel':
-            print("Empate!!")
-        elif user == 'papel' and programa == 'tesoura':
-            print("Você perdeu!!")
-            contadorProg += 1
-        elif user == 'papel' and programa == 'pedra':
-            print("Você ganhou!!")
-            contadorUser += 1
-
-        elif user == 'tesoura' and programa == 'tesoura':
-            print("Empate!!")
-        elif user == 'tesoura' and programa == 'papel':
-            print("Você ganhou!!")
-            contadorUser += 1
-        elif user == 'tesoura' and programa == 'pedra':
-            print("Você perdeu!!")
-            contadorProg += 1
-
+        if user == programa:
+            print("Empate")
         else:
-            print("Resposta incompatível!!")
-            
+            res = f"{user} x {programa}"
+            result(res,"compuPersons")
+
         print(f"Seus pontos -> {contadorUser} \nPontos computador -> {contadorProg}")
 
         if contadorProg == 5:
@@ -137,7 +114,7 @@ def gamePc():
         
         opcao = input("Digite 0 se quiser sair ou qualquer outra tecla se deseja continuar: ")
         if opcao == "0":
-            print("Até a próxima!")
+            print("Jogo encerrado... Até a próxima!")
             break
 
 if __name__ == "__main__":

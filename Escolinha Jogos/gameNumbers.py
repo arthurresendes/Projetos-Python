8512b25117164f07cb6bb869b2c440fa5664a038
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-20 11:43:30 -0300

    Otimizando codigo e reduzindo suas linhas repetitivas

--- Escolinha Jogos/gameNumbers.py
+++ Escolinha Jogos/gameNumbers.py
@@ -1,12 +1,5 @@
 import random
 
-'''
-Na função menu temos uma leve de opções dos 4 metodos de operação matematico de teste : +,-,*,/
-
-Abrimos um while para caso o user digitar uma opção que não esta entre 1 e 4 e tambem para caso não digite um numero inteiro
-
-Por fim chamamos as funções de cada modo de jogo
-'''
 def menu():
     print("--------------------------------")
     print("Bem-vindo ao jogo do adivinha\nEscolha uma das opções")
@@ -35,10 +28,6 @@ def menu():
     else:
         print("Opção invalida!!")
 
-'''
-Aqui onde é decidido o nível de jogo entre fácil ate o extreme e novamente temos um while para caso o user digite o numero errado
-
-'''
 def nivel():
     print("--------------------------")
     print("Escolha o nível do jogo: ")
@@ -50,289 +39,75 @@ def nivel():
         try:
             opcaoNivel = int(input(": "))
             if 1 <= opcaoNivel <= 4:
-                return opcaoNivel # Aqui retorna a opção nivel para podermos utilizar como base nas funções
+                return opcaoNivel
             else:
                 print("Digite um número entre 1 e 4!")
         except ValueError:
             print("Digite um número inteiro válido!")
 
-'''
-Aqui o jogo funciona como , o user tem que adivinhar o numero faltante entre os numeros que da o devido resultado 
-'''
-lista_resposta = []
-def adicao():
-    nivelEscolhido = nivel() # Aqui onde puxamos o return opcao nivel para saber qual nivel foi escolhido
+
+def nivelamento(nivel: int):
+    if nivel == 1:
+        return random.randint(1,10), random.randint(1,10)
+    elif nivel == 2:
+        return random.randint(1,100), random.randint(1,100)
+    elif nivel == 3:
+        return random.randint(1,1000), random.randint(1,1000)
+    elif nivel == 4:
+        return random.randint(1,10000), random.randint(1,10000)
+    else:
+        return "Erro ao encontrar nivel"
+
+def jogo(nivelEscolhido:int,operacao: str):
     contadorAcertos = 0
     contadorWhile = 0
-    if nivelEscolhido == 1:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10)
-            n2 = random.randint(1,10)
-            res = n1 + n2
+    while contadorWhile < 3:
+            n1, n2 = nivelamento(nivelEscolhido)
+            res = operacoes(operacao,n1,n2)
             lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} + ? = {res}: ")
-            resposta = int(input(": "))
+            print(f"Qual o número faltante de {n1} {operacao} ? = {res}: ")
+            resposta = float(input(": "))
             contadorWhile += 1
             if resposta == n2:
                 contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
+    print("----Fim de jogo----")
+    print(f"Você acertou {contadorAcertos} exercicios!!")
+    print(f"As respostas certas eram {lista_resposta}")
 
-    elif nivelEscolhido == 2:
-        while contadorWhile < 3:
-            n1 = random.randint(1,100)
-            n2 = random.randint(1,100)
-            res = n1 + n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} + ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile +=1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 3:
-        while contadorWhile < 3:
-            n1 = random.randint(1,1000)
-            n2 = random.randint(1,1000)
-            res = n1 + n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} + ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 4:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10000)
-            n2 = random.randint(1,10000)
-            n3 = random.randint(1,10000)
-            res = n1 + n2 + n3
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} + ? + {n3} = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
+def operacoes(operador: str,n1,n2):
+    if operador == '+':
+        return n1+n2
+    elif operador == '-':
+        return n1-n2
+    elif operador == '*':
+        return n1*n2
+    elif operador == '/':
+        return n1/n2
     else:
-        print("Opção invalida!!")
+        return "Erro"
+
+lista_resposta = []
+nivelEscolhido = nivel()
+
+#--------------------------------------
+# Adição
+def adicao():
+    jogo(nivelEscolhido,'+')
 
 # -------------------------------------
 # Subtração
 def sub():
-    nivelEscolhido = nivel()
-    contadorAcertos = 0
-    contadorWhile = 0
-    if nivelEscolhido == 1:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10)
-            n2 = random.randint(1,10)
-            res = n1 - n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} - ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 2:
-        while contadorWhile < 3:
-            n1 = random.randint(1,100)
-            n2 = random.randint(1,100)
-            res = n1 - n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} - ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile +=1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 3:
-        while contadorWhile < 3:
-            n1 = random.randint(1,1000)
-            n2 = random.randint(1,1000)
-            res = n1 - n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} - ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 4:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10000)
-            n2 = random.randint(1,10000)
-            n3 = random.randint(1,10000)
-            res = n1 - n2 - n3
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} - ? - {n3} = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-    else:
-        print("Opção invalida!!")
+    jogo(nivelEscolhido,'-')
 
 # -------------------------------------
 # Multiplicação
 def multi():
-    nivelEscolhido = nivel()
-    contadorAcertos = 0
-    contadorWhile = 0
-    if nivelEscolhido == 1:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10)
-            n2 = random.randint(1,10)
-            res = n1 * n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} x ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 2:
-        while contadorWhile < 3:
-            n1 = random.randint(1,100)
-            n2 = random.randint(1,100)
-            res = n1 * n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} x ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile +=1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 3:
-        while contadorWhile < 3:
-            n1 = random.randint(1,1000)
-            n2 = random.randint(1,1000)
-            res = n1 * n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} x ? = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 4:
-        while contadorWhile < 3:
-            n1 = random.randint(1,1000)
-            n2 = random.randint(1,1000)
-            n3 = random.randint(1,1000)
-            res = n1 * n2 * n3
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} x ? x {n3} = {res}: ")
-            resposta = int(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-    else:
-        print("Opção invalida!!")
+    jogo(nivelEscolhido,'*')
 
 # -------------------------------------
 # Divisão
 def divisao():
-    nivelEscolhido = nivel()
-    contadorAcertos = 0
-    contadorWhile = 0
-    if nivelEscolhido == 1:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10)
-            n2 = random.randint(1,10)
-            res = n1 / n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} / ? = {res}: ")
-            resposta = float(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
+    jogo(nivelEscolhido,'/')
 
-    elif nivelEscolhido == 2:
-        while contadorWhile < 3:
-            n1 = random.randint(1,100)
-            n2 = random.randint(1,100)
-            res = n1 / n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} / ? = {res}: ")
-            resposta = float(input(": "))
-            contadorWhile +=1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 3:
-        while contadorWhile < 3:
-            n1 = random.randint(1,1000)
-            n2 = random.randint(1,1000)
-            res = n1 / n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} / ? = {res}: ")
-            resposta = float(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-
-    elif nivelEscolhido == 4:
-        while contadorWhile < 3:
-            n1 = random.randint(1,10000)
-            n2 = random.randint(1,10000)
-            res = n1 / n2
-            lista_resposta.append(n2)
-            print(f"Qual o número faltante de {n1} / ? = {res}: ")
-            resposta = float(input(": "))
-            contadorWhile += 1
-            if resposta == n2:
-                contadorAcertos += 1
-        print("----Fim de jogo----")
-        print(f"Você acertou {contadorAcertos} exercicios!!")
-        print(f"As respostas certas eram {lista_resposta}")
-    else:
-        print("Opção invalida!!")
-
-# O menu onde eh tudo começado e puxa as funções roda no comando principal main
 if __name__ == "__main__":
     menu()
